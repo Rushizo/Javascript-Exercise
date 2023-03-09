110f9ac209aedd1293bd560b0fd0202ea78b41c5
Danilo Innocente Carbone <danilocarbone2015@gmail.com>
2023-03-09 18:08:57 +0100

    Exercise 9 done

---
+++ Esercizio9/exercise-1.js
@@ -0,0 +1,12 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+// Print values of person using Object.keys
+
+
+Object.keys(person).forEach((keys) => {
+    console.log(`${keys} : ${person[keys]}`)
+});

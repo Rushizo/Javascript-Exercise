f4bee69ea5b62644602b4c5bdcc04fc1fd329b83
Danilo Innocente Carbone <danilocarbone2015@gmail.com>
2023-03-09 17:32:26 +0100

    Exercise 7 done

---
+++ Esercizio7/exercise.js
@@ -0,0 +1,12 @@
+function printName(){
+    let helloName = "Hello John"
+    function inner(){
+        setTimeout(function(){
+           console.log(helloName);
+        }, 1000);
+    }
+    return inner;
+};
+
+
+printName()();

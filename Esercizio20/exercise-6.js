72d0dddf328aced1ea2c3978959f9694f3db21e7
Danilo Innocente Carbone <danilocarbone2015@gmail.com>
2023-03-17 18:44:23 +0100

    Exercise 20 done

---
+++ Esercizio20/exercise-6.js
@@ -0,0 +1,38 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate(geometricFigure){
+    if(geometricFigure instanceof Square){
+      return geometricFigure.side * geometricFigure.side;
+    } else if (geometricFigure instanceof Circle){
+      return Math.PI * (geometricFigure.radius * geometricFigure.radius)
+    } else if (geometricFigure instanceof Rectangle){
+      return geometricFigure.width * geometricFigure.height;
+    }
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));

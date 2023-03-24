a1eb41b1e6ccabca4c4877e85138b68fe8b6aa9a
Danilo Innocente Carbone <danilocarbone2015@gmail.com>
2023-03-24 20:22:41 +0100

    Exercise 22 done

---
+++ Esercizio22/exercise-2.js
@@ -0,0 +1,15 @@
+// const person = {
+//   id: 1,
+//   firstName: 'Mario',
+//   lastName: 'Rossi',
+//   age: 25
+// };
+
+// const id = person.id;
+// const firstName = person.firstName;
+// const lastName = person.lastName;
+// const age = person.age;
+
+[id, firstName, lastName, age] = [1, "Mario", "Rossi", 25];
+
+console.log(id, firstName, lastName, age);

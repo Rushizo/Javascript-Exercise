793bb455f026d8c5bacd02c4f5531410947505ab
Danilo Innocente Carbone <danilocarbone2015@gmail.com>
2023-04-11 17:51:47 +0200

    Exercise 35 done

--- Esercizio35/exercise.js
+++ Esercizio35/exercise.js
@@ -1,6 +1,5 @@
-function repeatHello(callback, n){
+function repeatHello(callback){
     let id = setInterval(callback, 1000);
-    setTimeout(() => clearInterval(id), 1000 * n)
 }
 
 repeatHello(() => console.log("Hello"), 5);
